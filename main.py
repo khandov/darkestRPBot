1b67fa7fa80f02e59b5399bcca3d1da90069c426
khandov <akhandov@gmail.com>
2024-12-17 20:29:42 +0100

    fix main

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import subprocess
 import statsdb.dbscript as db
-import registerCommands
 def run_script(script_name):
     subprocess.run(['python', script_name])
 db.initiate()
 if __name__ == "__main__":
-    scripts = ['registerCommands.py']
+    scripts = ['api/commands.py']
     for script in scripts:
         run_script(script)
